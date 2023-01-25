876ec6b1ca028810a4a0cc8f95a784e9c8a6c7e5
mialuzmia <mialuzazevedo@gmail.com>
2023-01-25 16:09:37 -0400

    novos exercícios

---
+++ aula13/testewhile.js
@@ -0,0 +1,14 @@
+let cont = 1 
+
+do{
+    console.log(`Passo ${cont} `)
+    cont = cont + 1
+}while(cont <= 10)
+
+
+/* let c = 1 
+
+while(c <= 10){
+    console.log(`Passo ${c} `)
+    c++
+} */
---
+++ aula14/testeFor.js
@@ -0,0 +1,7 @@
+console.log('Vai começar!')
+
+for(tcon = 1; cont <= 10; cont++){
+    console.log(cont)
+}
+
+console.log('Fim!')
