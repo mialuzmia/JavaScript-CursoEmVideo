dcfb43078a03e0fe2cc908db101d64e459f738f2
mialuzmia <mialuzazevedo@gmail.com>
2023-01-27 15:28:43 -0400

    contador terminado

--- aula14ex/ex016-Vamos-Contar/script.js
+++ aula14ex/ex016-Vamos-Contar/script.js
@@ -9,18 +9,36 @@ function contar(){
 
     if(txtinicio.value.length == 0 || txtfim.value.length == 0 || txtpulo.value.length == 0){ 
         alert('[ERRO] FALTAM DADOS!') 
+        res.innerHTML = "Impossível contar!"
     }   
         else{
-            res.innerHTML = 'Contando: '
+            res.innerHTML = 'Contando: <br>'
 
             let inicio = Number(txtinicio.value)
             let fim = Number(txtfim.value)
             let pulo = Number(txtpulo.value)
 
-            for(let cont = inicio; cont <= fim; cont = cont + pulo){
-                res.innerHTML += `${cont} \u{1F449} `
+            if (pulo <= 0){
+                alert('Pulo inválido, considerando pulo 1')
+                pulo  = 1
             }
+            if(inicio < fim){
+                // crescente 
+                for(let cont = inicio; cont <= fim; cont = cont + pulo){
+                    res.innerHTML += `${cont} \u{1F449} `
+                }
+                
+            }
+            else if(inicio > fim){
+                //decrescente
+                for(let cont = inicio; cont >= fim; cont = cont - pulo){
+                    res.innerHTML += `${cont} \u{1F449}`
+                }
+                
+            }
+            
             res.innerHTML += `\u{1F3F4} `
+            
         }
 
   
